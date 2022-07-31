c393ac0c06691fa7dd3703c6157ee19e1fe164fb
Dmitrii <volkovdmvd@gmail.com>
2022-07-31 12:18:14 +0400

    ---

--- utils/split_picture.py
+++ utils/split_picture.py
@@ -9,18 +9,18 @@ pts = np.array([[10,150],[150,100],[300,150],[350,100],[310,20],[35,10]])
 
 def crop_one_detail(image, points):
 
-    ## (1) Crop the bounding rect
+    # Crop the bounding rect
     rect = cv2.boundingRect(points)
     x, y, w, h = rect
     cropped = image[y:y+h, x:x+w].copy()
 
-    ## (2) make mask
+    # Make mask
     pts = points - points.min(axis=0)
 
     mask = np.zeros(cropped.shape[:2], np.uint8)
     cv2.drawContours(mask, [pts], -1, (255, 255, 255), -1, cv2.LINE_AA)
 
-    ## (3) do bit-op
+    # Do bit-op
     dst = cv2.bitwise_and(cropped, cropped, mask=mask)
 
     tmp = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
@@ -28,11 +28,11 @@ def crop_one_detail(image, points):
     b, g, r = cv2.split(dst)
     rgba = [b, g, r, alpha]
     dst = cv2.merge(rgba, 4)
-    cv2.imwrite("transparent_part.png", dst)
+    # cv2.imwrite("transparent_part.png", dst)
     return dst
 
 
-crop_one_detail(img, pts)
+# crop_one_detail(img, pts)
 
 
 def split_image(image, list_points):
@@ -40,10 +40,10 @@ def split_image(image, list_points):
     for points in list_points:
         part = crop_one_detail(image, points)
         parts.append(part)
-        cv2.imwrite(str(random.randint(0,1000))+'cropeee.png', part)
+        # cv2.imwrite(str(random.randint(0,1000))+'cropeee.png', part)
     return parts
 
 
-split_image(img, [np.array([[10,150],[150,100],[300,150],[350,100],[310,20],[35,10]]),
-                  np.array([[10,150],[150,100],[300,150],[350,100],[310,400],[0,400]]),
-                  ], )
+# split_image(img, [np.array([[10,150],[150,100],[300,150],[350,100],[310,20],[35,10]]),
+#                   np.array([[10,150],[150,100],[300,150],[350,100],[310,400],[0,400]]),
+#                   ], )
